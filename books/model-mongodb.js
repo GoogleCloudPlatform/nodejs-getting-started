64cea65af8c6c958faf586a68a36f451088e7ab4
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-06-07 11:24:00 -0700

    Properly handling err in model-mongodb.js:list

--- books/model-mongodb.js
+++ books/model-mongodb.js
@@ -1,15 +1,15 @@
 /*
-	Copyright 2015, Google, Inc. 
- Licensed under the Apache License, Version 2.0 (the "License"); 
- you may not use this file except in compliance with the License. 
- You may obtain a copy of the License at 
-  
-    http://www.apache.org/licenses/LICENSE-2.0 
-  
- Unless required by applicable law or agreed to in writing, software 
- distributed under the License is distributed on an "AS IS" BASIS, 
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- See the License for the specific language governing permissions and 
+  Copyright 2015, Google, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
  limitations under the License.
 */
 "use strict";
@@ -40,7 +40,10 @@ module.exports = function(config, background) {
 
   function getCollection(cb) {
     MongoClient.connect(url, function(err, db) {
-      if (err) return cb(err);
+      if (err) {
+        console.log(err);
+        return cb(err);
+      }
       cb(null, db.collection(collectionName));
     });
   }
@@ -49,6 +52,7 @@ module.exports = function(config, background) {
   function list(limit, token, cb) {
     token = token ? parseInt(token, 10) : 0;
     getCollection(function(err, collection) {
+      if (err) return cb(err);
       collection.find({})
         .skip(token)
         .limit(limit)
