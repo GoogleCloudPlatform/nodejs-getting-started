2a5450f932630a97dce9e391cce7103259cf2cc1
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-08-25 13:35:16 -0700

    Moving to nodejs runtime

--- worker.js
+++ worker.js
@@ -1,15 +1,15 @@
 /*
-	Copyright 2015, Google, Inc. 
- Licensed under the Apache License, Version 2.0 (the "License"); 
- you may not use this file except in compliance with the License. 
- You may obtain a copy of the License at 
-  
-    http://www.apache.org/licenses/LICENSE-2.0 
-  
- Unless required by applicable law or agreed to in writing, software 
- distributed under the License is distributed on an "AS IS" BASIS, 
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- See the License for the specific language governing permissions and 
+	Copyright 2015, Google, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
  limitations under the License.
 */
 "use strict";
@@ -40,14 +40,16 @@ var bookCount = 0;
 
 /*
   When running on Google App Engine Managed VMs, the worker needs
-  to respond to health checks. We can re-use the health checks
-  from the main application and create a simple server.
+  to respond to HTTP requests and can optionally supply a health check.
 */
 // [START server]
 var app = express();
 
 app.use(logging.requestLogger);
-app.use(require('./lib/appengine-handlers'));
+
+app.get('/_ah/health', function(req, res) {
+  res.status(200).send('ok');
+});
 
 app.get('/', function(req, res) {
   res.send('This worker has processed ' + bookCount + ' books.');
