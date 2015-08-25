109117b808dd479b22da7d1cd56ad9bbce5624fc
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-08-25 13:33:58 -0700

    Moving to nodejs runtime

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 /*
-	Copyright 2015, Google, Inc. 
- Licensed under the Apache License, Version 2.0 (the "License"); 
- you may not use this file except in compliance with the License. 
- You may obtain a copy of the License at 
-  
-    http://www.apache.org/licenses/LICENSE-2.0 
-  
- Unless required by applicable law or agreed to in writing, software 
- distributed under the License is distributed on an "AS IS" BASIS, 
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
- See the License for the specific language governing permissions and 
+	Copyright 2015, Google, Inc.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
  limitations under the License.
 */
 "use strict";
@@ -50,25 +50,22 @@ app.use(session({
 }));
 
 
-/* Include the app engine handlers to respond to start, stop, and health checks. */
-app.use(require('./lib/appengine-handlers'));
-
-
 /* OAuth2 */
 var oauth2 = require('./lib/oauth2')(config.oauth2);
-
 app.use(oauth2.router);
 
 
-/* Books */
+// Setup modules and dependencies
 var images = require('./lib/images')(config.gcloud, config.cloudStorageBucket);
 var model = require('./books/model-' + config.dataBackend)(config);
 
+
+// Books
 app.use('/books', require('./books/crud')(model, images, oauth2));
 app.use('/api/books', require('./books/api')(model));
 
 
-/* Redirect root to /books */
+// Redirect root to /books
 app.get('/', function(req, res) {
   res.redirect('/books');
 });
@@ -83,7 +80,8 @@ app.get('/', function(req, res) {
 app.use(logging.errorLogger);
 // [END errors]
 
-/* Start the server */
+
+// Start the server
 var server = app.listen(config.port, '0.0.0.0', function() {
   console.log('App listening at http://%s:%s', server.address().address, server.address().port);
   console.log("Press Ctrl+C to quit.");
