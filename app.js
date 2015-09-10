0a371ac17af7c2a9c7ea8627e0d36a12814036c8
Jon Wayne Parrott <jon.wayne.parrott@gmail.com>
2015-09-10 10:21:21 -0700

    Basic hello world example.

---
+++ app.js
@@ -0,0 +1,37 @@
+// Copyright 2015, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+var express = require('express');
+
+var app = express();
+
+
+// [START hello_world]
+// Say hello!
+app.get('/', function(req, res) {
+  res.status(200).send('Hello, world!');
+});
+// [END hello_world]
+
+
+// [START server]
+// Start the server
+var server = app.listen(process.env.PORT || 8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('App listening at http://%s:%s', host, port);
+});
+// [END server]
