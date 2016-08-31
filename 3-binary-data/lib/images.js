6ba7453657ec246cc831e932fe57d414b316c8c5
André Cipriani Bandarra <andreban@gmail.com>
2016-08-31 18:16:59 +0100

    Set the Content-Type of uploaded images (#67)
    Gets the Content-Type from multer and sets it when uploading the
    image..

--- 3-binary-data/lib/images.js
+++ 3-binary-data/lib/images.js
@@ -44,7 +44,12 @@ function sendUploadToGCS (req, res, next) {
 
   var gcsname = Date.now() + req.file.originalname;
   var file = bucket.file(gcsname);
-  var stream = file.createWriteStream();
+
+  var stream = file.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype
+    }
+  });
 
   stream.on('error', function (err) {
     req.file.cloudStorageError = err;
