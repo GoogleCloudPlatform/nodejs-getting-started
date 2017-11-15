e24e28d53a9074c7e34ee3993ee9a3f5c05f8860
Ace Nassri <anassri@google.com>
2017-11-14 18:25:31 -0800

    Debug: use config-file URLs

--- 7-gce/test/worker.test.js
+++ 7-gce/test/worker.test.js
@@ -18,7 +18,9 @@ const path = require(`path`);
 const proxyquire = require(`proxyquire`);
 const sinon = require(`sinon`);
 const test = require(`ava`);
-const utils = require(`@google-cloud/nodejs-repo-tools`);
+const utils = {
+  getRequest: c => c.testUrl
+};
 
 test.serial.cb(`should return number of processed books`, (t) => {
   utils.getRequest(testConfig)
