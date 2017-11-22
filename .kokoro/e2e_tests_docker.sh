c7f7fb1640a5a4106d9861028853bae436a7ef68
Ace Nassri <anassri@google.com>
2017-11-21 17:39:07 -0800

    Tweak wait logic to prevent '<pending>' triggering it

--- .kokoro/e2e_tests_docker.sh
+++ .kokoro/e2e_tests_docker.sh
@@ -95,11 +95,12 @@ kubectl create -f bookshelf-worker.yaml
 kubectl create -f bookshelf-service.yaml
 
 # Wait for services to initialize
-until kubectl get services | awk '{ print $4 }' | grep -E "(\\d|\\.)+"
+sleep 30;
+until kubectl get services | awk '{ print $4 }' | grep -E "^(\\d|\\.)+$"
 do
   sleep 30;
 done
-export TEST_URL=http://$(kubectl get services | awk '{ print $4 }' | grep -E "(\d|\.)+")
+export TEST_URL=http://$(kubectl get services | awk '{ print $4 }' | grep -E "^(\d|\.)+$")
 
 # Run (only) the tests that GKE supports
 export SKIP_WORKER_HTTP_TESTS=True
