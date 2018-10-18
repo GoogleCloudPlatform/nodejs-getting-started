8156ac67ccdb586dbd6ab29c24e24a1e6327c36a
Ace Nassri <anassri@google.com>
2018-10-18 11:17:48 -0700

    Fix scripts + use nvm + encrypt config

--- 7-gce/gce/startup-script.sh
+++ 7-gce/gce/startup-script.sh
@@ -1,6 +1,6 @@
 
 #! /bin/bash
-#	Copyright 2017, Google, Inc.
+#	Copyright 2018, Google LLC.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -15,6 +15,7 @@
 
 # [START startup]
 set -v
+export HOME=/root
 
 # Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
@@ -28,27 +29,41 @@ service google-fluentd restart &
 
 # Install dependencies from apt
 apt-get update
-apt-get install -yq ca-certificates git nodejs build-essential supervisor
+apt-get install -yq ca-certificates git build-essential supervisor
 
-# Install nodejs
-mkdir /opt/nodejs
-curl https://nodejs.org/dist/v4.2.2/node-v4.2.2-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
-ln -s /opt/nodejs/bin/node /usr/bin/node
-ln -s /opt/nodejs/bin/npm /usr/bin/npm
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node 8
+nvm install 8
+nvm use 8
 
 # Get the application source code from the Google Cloud Repository.
 # git requires $HOME and it's not set during the startup script.
 export HOME=/root
 git config --global credential.helper gcloud.sh
-git clone https://source.developers.google.com/p/$PROJECTID /opt/app
+git clone https://source.developers.google.com/p/$PROJECTID/r/gce-bookshelf /opt/app
+
+# Decrypt the config.json file
+gcloud kms decrypt --location=global --keyring=[YOUR_KEY_RING] --key=[YOUR_KEY_NAME] --plaintext-file=config.json --ciphertext-file=config.json.enc
 
 # Install app dependencies
 cd /opt/app/7-gce
-npm install
+npm install --unsafe-perm
+npm rebuild grpc
 
 # Create a nodeapp user. The application will run as this user.
 useradd -m -d /home/nodeapp nodeapp
 chown -R nodeapp:nodeapp /opt/app
+
+# Expose node.js installation to the nodeapp user
+NODE_PATH=$(dirname $(which node))
+chmod a+rx /root
+chmod -R a+rx $NODE_PATH
+ln -s $NODE_PATH/npm /usr/bin/npm
+ln -s $NODE_PATH/node /usr/bin/node
 
 # Configure supervisor to run the node app.
 cat >/etc/supervisor/conf.d/node-app.conf << EOF
