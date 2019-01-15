c22fe1488d721766350b36d1173672f9470965bc
Naresh Kumar <naresh@qlogic.io>
2019-01-16 01:53:36 +0530

    refactor(binary-data): convert ava tests to mocha #256

--- 3-binary-data/test/_test-config.js
+++ 3-binary-data/test/_test-config.js
@@ -13,15 +13,15 @@
 
 'use strict';
 
-const path = require(`path`);
+const path = require('path');
 
-const TESTNAME = `3-binary-data`;
+const TESTNAME = '3-binary-data';
 const PORT = 8083;
 
 module.exports = {
   test: TESTNAME,
-  cwd: path.resolve(path.join(__dirname, `../`)),
-  cmd: `app`,
+  cwd: path.resolve(path.join(__dirname, '../')),
+  cmd: 'app',
   port: PORT,
   env: {
     PORT: PORT,
@@ -29,5 +29,5 @@ module.exports = {
   url: `http://localhost:${PORT}`,
   version: process.env.GAE_VERSION || TESTNAME,
   project: process.env.GCLOUD_PROJECT,
-  msg: `Bookshelf`,
+  msg: 'Bookshelf',
 };
