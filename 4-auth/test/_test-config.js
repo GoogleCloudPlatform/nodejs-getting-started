b64a7be9ea6b2ae2a9341717f35ab5017c0f7333
Naresh Kumar <naresh@qlogic.io>
2019-01-16 02:02:27 +0530

    refactor(auth): convert ava tests to mocha #256

--- 4-auth/test/_test-config.js
+++ 4-auth/test/_test-config.js
@@ -13,15 +13,15 @@
 
 'use strict';
 
-const path = require(`path`);
+const path = require('path');
 
-const TESTNAME = `4-auth`;
+const TESTNAME = '4-auth';
 const PORT = 8084;
 
 module.exports = {
   test: TESTNAME,
-  cwd: path.resolve(path.join(__dirname, `../`)),
-  cmd: `app`,
+  cwd: path.resolve(path.join(__dirname, '../')),
+  cmd: 'app',
   port: PORT,
   env: {
     PORT: PORT,
@@ -29,5 +29,5 @@ module.exports = {
   url: `http://localhost:${PORT}`,
   version: process.env.GAE_VERSION || TESTNAME,
   project: process.env.GCLOUD_PROJECT,
-  msg: `Bookshelf`,
+  msg: 'Bookshelf',
 };
