90a4cdbfb1d6f2bccc7d52d888ed1ee3e3870c7c
F. Hinkelmann <franziska.hinkelmann@gmail.com>
2019-08-05 14:38:25 -0400

    Initial import for tutorial for handling sessions (#332)

---
+++ sessions/index.js
@@ -0,0 +1,49 @@
+// Copyright 2019, Google LLC.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const {Firestore} = require('@google-cloud/firestore');
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const {FirestoreStore} = require('@google-cloud/connect-firestore');
+
+app.use(
+  session({
+    store: new FirestoreStore({
+      dataset: new Firestore({
+        kind: 'express-sessions',
+      }),
+    }),
+    secret: 'my-secret',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+const colors = ['red', 'blue', 'green', 'yellow', 'pink'];
+
+app.get('/', (req, res) => {
+  console.log(req.session.id);
+  if (!req.session.views) {
+    req.session.views = 0;
+    req.session.color = colors[Math.floor(Math.random() * 5)];
+  }
+  const views = req.session.views++;
+  res.send(`<body bgcolor=${req.session.color}>Views ${views}</body>`);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
