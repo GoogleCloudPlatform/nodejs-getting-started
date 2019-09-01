674dec24b9f28789c9b6a2c3443a9cc9fa2f00e9
Charles Engelke <engelke@google.com>
2019-09-01 07:46:54 -0700

    Sample for user auth with IAP (#334)

---
+++ authenticating-users/test/app.test.js
@@ -0,0 +1,16 @@
+const app = require('../app');
+
+const request = require('supertest');
+
+describe('GET /', () => {
+  it('should get 200', done => {
+    request(app)
+      .get('/')
+      .expect(200, done);
+  }),
+    it('should get Hello None', done => {
+      request(app)
+        .get('/')
+        .expect('Hello None', done);
+    });
+});
