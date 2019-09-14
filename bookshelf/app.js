e7f57ff1e0bd4d19dd18fdc1f0c02f46b1befdd6
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2019-09-14 17:50:19 -0400

    Simplify app.js

--- bookshelf/app.js
+++ bookshelf/app.js
@@ -13,15 +13,12 @@
 
 'use strict';
 
-const path = require('path');
 const express = require('express');
 
 const app = express();
 
-app.disable('etag');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', require('path').join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.set('trust proxy', true);
 
 // Books
 app.use('/books', require('./books/crud'));
@@ -32,25 +29,10 @@ app.get('/', (req, res) => {
   res.redirect('/books');
 });
 
-// Basic 404 handler
-app.use((req, res) => {
-  res.status(404).send('Not Found');
-});
-
-// Basic error handler
-app.use((err, req, res) => {
-  /* jshint unused:false */
-  console.error(err);
-  // If our routes specified a specific response, then send that. Otherwise,
-  // send a generic message so as not to leak anything.
-  res.status(500).send(err.response || 'Something broke!');
-});
-
 const port = process.env.PORT || 8080;
 
 // Start the server
-const server = app.listen(port, () => {
-  const port = server.address().port;
+app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
 
