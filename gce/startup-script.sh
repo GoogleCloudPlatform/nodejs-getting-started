50d546a473d80c41c7ddc72f3b8bfe168dad01a4
Sofia Leon <sofialeon@google.com>
2020-05-30 02:25:07 -0700

    include startup-script changes

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -17,14 +17,12 @@ set -v
 
 # Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
-REPOSITORY="[YOUR-REPOSITORY]"
+REPOSITORY="new-repo"
 
 # Install logging monitor. The monitor will automatically pick up logs sent to
 # syslog.
-# [START logging]
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
-# [END logging]
 
 # Install dependencies from apt
 apt-get update
@@ -43,7 +41,7 @@ git config --global credential.helper gcloud.sh
 git clone https://source.developers.google.com/p/${PROJECTID}/r/${REPOSITORY} /opt/app
 
 # Install app dependencies
-cd /opt/app/gce
+cd /opt/app/new-repo
 npm install
 
 # Create a nodeapp user. The application will run as this user.
@@ -53,10 +51,11 @@ chown -R nodeapp:nodeapp /opt/app
 # Configure supervisor to run the node app.
 cat >/etc/supervisor/conf.d/node-app.conf << EOF
 [program:nodeapp]
-directory=/opt/app/gce
+directory=/opt/app/new-repo
 command=npm start
 autostart=true
 autorestart=true
+startretries=10
 user=nodeapp
 environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production"
 stdout_logfile=syslog
