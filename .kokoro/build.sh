ee33d49225c14a5b331dd740e30ab1abab2fb2de
Jeff Ching <chingor@google.com>
2021-11-03 16:41:13 -0700

    build: build.sh should fail early if any command fails

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 export GCLOUD_PROJECT='firestore-nodejs-getting-start'
 export REGION_ID='uc'
 
