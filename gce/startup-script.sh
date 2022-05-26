894cc9409e27f17a05c43385b4e825d6c18ac563
Jason Borg <jcborg@google.com>
2022-05-26 20:27:16 +0000

    Update GCE sample to Node.js v16.15.0
    Updates the GCE sample app to use Node.js v16.15.0. Currently it uses
    v8.12.0, which is no longer supported.

--- gce/startup-script.sh
+++ gce/startup-script.sh
@@ -34,7 +34,7 @@ apt-get install -yq ca-certificates git build-essential supervisor
 
 # Install nodejs
 mkdir /opt/nodejs
-curl https://nodejs.org/dist/v8.12.0/node-v8.12.0-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
+curl https://nodejs.org/dist/v16.15.0/node-v16.15.0-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
 ln -s /opt/nodejs/bin/node /usr/bin/node
 ln -s /opt/nodejs/bin/npm /usr/bin/npm
 
